e88e01b1d756e0bca2f790e0a7aac6482ee59736
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-23 13:24:05 +0530

    [Akash] Add : Welcome employee wage computation

---
+++ Employee-wage-computation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_wage_computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+        }
+    }
+}
