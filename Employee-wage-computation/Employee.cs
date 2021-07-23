d28559d347ea189eb956d365f163e047db2a1f41
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-23 13:45:18 +0530

    [Akash] Add : Calculate part time and full time wage

--- Employee-wage-computation/Employee.cs
+++ Employee-wage-computation/Employee.cs
@@ -9,15 +9,25 @@ namespace Employee_wage_computation
         //variable
         int totalWage;
         //constant
-        const int WAGE_PER_HOUR = 20;
-        const int FULL_DAY_HOUR = 8;
-        /// <summary>
-        /// calculate employee daily wage
-        /// </summary>
+        const int WAGE_PER_HOUR = 20, FULL_DAY_HOUR = 8, HALF_DAY_HOUR = 4, FULL_DAY = 0;
+
+       /// <summary>
+       /// Calculate employee part time and full time wage
+       /// </summary>
         public void EmployeeWage()
         {
-            totalWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-            Console.WriteLine("Employee Daily Wage is :" + totalWage);
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == FULL_DAY)
+            {
+                totalWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+            }
+            else
+            {
+                totalWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+            }
+
+            Console.WriteLine("Employee Wage is :" + totalWage);
         }
     }
 }
